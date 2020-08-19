c9094bd6eb585cc68cd9e0ecd29c80c313590083
NightJolt <mk.ff.dev@gmail.com>
2020-08-19 16:56:10 +0400

    Added FTime and Noise

---
+++ header-files/FTime.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <SFML/System.hpp>
+
+using namespace sf;
+
+class FTime {
+public:
+    static void Recalculate();
+    static float DeltaTime();
+    static float TimeElapsed();
+
+    static float time_slow;
+
+private:
+    static Clock runtime_clock;
+    static Clock delta_clock;
+
+    static float delta_time;
+};
---
+++ source-files/FTime.cpp
@@ -0,0 +1,19 @@
+#include "FTime.h"
+
+Clock FTime::runtime_clock = Clock();
+Clock FTime::delta_clock = Clock();
+
+float FTime::delta_time = 0;
+float FTime::time_slow = 1;
+
+void FTime::Recalculate() {
+    delta_time = delta_clock.restart().asSeconds();
+}
+
+float FTime::DeltaTime() {
+    return delta_time;
+}
+
+float FTime::TimeElapsed() {
+    return runtime_clock.getElapsedTime().asSeconds();
+}
