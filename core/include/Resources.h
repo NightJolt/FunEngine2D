12b5c90fe3abf40c963456720ac24398d2f5765b
NightJolt <mk.ff.dev@gmail.com>
2021-12-12 16:58:37 +0400

    deoop modules

---
+++ core/include/Resources.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "globals.h"
+
+namespace fun {
+    void load_texture(const std::string&, const std::string&);
+    const sf::Texture& get_texture(const std::string&);
+
+    // void LoadImage(const std::string&, const std::string&);
+    // const sf::Image& GetImage(const std::string&);
+
+    void load_font(const std::string&, const std::string&);
+    const sf::Font& get_font(const std::string&);
+
+    // void LoadShader(const std::string&, const std::string&);
+    // const sf::Shader& GetShader(const std::string&);
+
+    // todo: audio
+    // todo: lua scripts
+
+    static std::unordered_map <std::string, sf::Texture> textures;
+    static std::unordered_map <std::string, sf::Font> fonts;
+    // static std::unordered_map <std::string, sf::Image> images;
+    // static std::unordered_map <std::string, sf::Shader*> shaders;
+
+    const std::string resources_directory =
+    #if defined(RELEASE_BUILD)
+    "/resources/"
+    #else
+    "../resources/"
+    #endif
+    ;
+
+    const std::string textures_directory = resources_directory + "textures/";
+    const std::string images_directory = resources_directory + "images/";
+    const std::string fonts_directory = resources_directory + "fonts/";
+    const std::string shaders_directory = resources_directory + "shaders/";
+}
---
+++ core/source/Resources.cpp
@@ -0,0 +1,55 @@
+#include "Resources.h"
+
+void fun::load_texture(const std::string& key, const std::string& file) {
+    textures.insert_or_assign(key, sf::Texture());
+
+    auto it = textures.find(key);
+
+    if (!it->second.loadFromFile(textures_directory + file)) {
+        textures.erase(it);
+    }
+}
+
+const sf::Texture& fun::get_texture(const std::string& key) {
+    return textures.at(key);
+}
+
+void fun::load_font(const std::string& key, const std::string& file) {
+    fonts.insert_or_assign(key, sf::Font());
+
+    auto it = fonts.find(key);
+
+    if (!it->second.loadFromFile(fonts_directory + file)) {
+        fonts.erase(it);
+    }
+}
+
+const sf::Font& fun::get_font(const std::string& key) {
+    return fonts.at(key);
+}
+
+// void LoadResources() {
+    // for (const auto& texture_path : textures_to_load) {
+    //     sf::Texture* texture = &textures.emplace_back(sf::Texture());
+
+    //     if (!texture->loadFromFile(textures_directory + texture_path)) {
+    //         textures.pop_back();
+    //     }
+    // }
+
+    // for (const auto& image_path : images_to_load) {
+    //     sf::Image* image = &images.emplace_back(sf::Image());
+
+    //     if (!image->loadFromFile(images_directory + image_path)) {
+    //         images.pop_back();
+    //     }
+    // }
+
+    // for (const auto& shader_path : shaders_to_load) {
+    //     sf::Shader* shader = shaders.emplace_back(new sf::Shader());
+
+    //     if (!shader->loadFromFile(shaders_directory + shader_path + ".vert", shaders_directory + shader_path + ".frag")) {
+    //         shaders.pop_back();
+    //     }
+    // }
+// }
