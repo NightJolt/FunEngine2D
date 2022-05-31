1eb6ee351221fb3187bb630ee68f88c10937353d
NightJolt <mk.ff.dev@gmail.com>
2022-06-01 02:11:24 +0400

    update

--- core/include/color.h
+++ core/include/color.h
@@ -4,12 +4,16 @@
 
 namespace fun {
     struct rgb_t {
+        static const rgb_t black;
+
         rgb_t();
         rgb_t(uint8_t, uint8_t, uint8_t);
 
         uint8_t r;
         uint8_t g;
         uint8_t b;
+
+        bool operator ==(const rgb_t&);
 
 #if defined(USES_SFML)
         rgb_t(sf::Color);
--- core/source/color.cpp
+++ core/source/color.cpp
@@ -1,7 +1,13 @@
 #include "color.h"
 
+const fun::rgb_t fun::rgb_t::black = { 0, 0, 0 };
+
 fun::rgb_t::rgb_t() : r(0), g(0), b(0) {}
 fun::rgb_t::rgb_t(uint8_t r, uint8_t g, uint8_t b) : r(r), g(g), b(b) {}
+
+bool fun::rgb_t::operator ==(const rgb_t& other) {
+    return r == other.r && g == other.g && b == other.b;
+}
 
 #if defined(USES_SFML)
 fun::rgb_t::rgb_t(sf::Color color) : r(color.r), g(color.g), b(color.b) {}
