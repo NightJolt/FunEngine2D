cd88bf47ee8594dc01700b24c6be12630e61823c
nightjolt <mk.ff.dev@gmail.com>
2023-12-29 02:46:14 +0400

    fixed typo

--- core/source/rpc/sync.cpp
+++ core/source/rpc/sync.cpp
@@ -1,15 +1,15 @@
 #include "rpc/sync.h"
 
 void fun::rpc::wait_for_sync_call_reply(connection_provider_t& connection_provider, std::function<void(deserializer_t&)> sync_call_data_extractor) {
-    uint32_t packed_ind = 0;
+    uint32_t packet_ind = 0;
     auto& packet_storage = connection_provider.get_packet_storage();
 
     while (true) {
         connection_provider.check_for_incoming_connections();
         connection_provider.check_for_incoming_data();
         
-        while (packed_ind < packet_storage.get_size()) {
-            auto& packet = packet_storage[packed_ind];
+        while (packet_ind < packet_storage.get_size()) {
+            auto& packet = packet_storage[packet_ind];
             deserializer_t deserializer(packet.get_data());
 
             oid_t call_type = deserializer.deserialize<oid_t>();
@@ -20,13 +20,13 @@ void fun::rpc::wait_for_sync_call_reply(connection_provider_t& connection_provid
                 if (answer_type == 1) {
                     sync_call_data_extractor(deserializer);
 
-                    packet_storage.remove(packed_ind);
+                    packet_storage.remove(packet_ind);
 
                     return;
                 }
             }
 
-            packed_ind++;
+            packet_ind++;
         }
     }
 }
